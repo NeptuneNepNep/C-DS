f3906f0ef5353660caae0dae536dbafaaee8186e
tom_glb <clearlove2300@163.com>
2022-06-05 10:36:09 +0800

    单链表和顺序表

---
+++ LinkList/LinkList/LinkList.cpp
@@ -0,0 +1,112 @@
+﻿// LinkList.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+typedef struct Node {
+	int data;
+	struct Node* next;
+
+
+}Node,*LinkList;
+void CreateNode(Node *&node,int data) {
+	node = (Node*)malloc(sizeof(Node));
+	node->data = data;
+	node->next = NULL;
+}
+bool InitList(LinkList &list) {
+	list = (Node*)malloc(sizeof(Node));
+	list->data = 0;
+	list->next = NULL;
+	return true;
+}
+bool ListInsert(LinkList &list, int i, int e) {
+	LinkList p = list;
+	for (int j = 0; j < i; j++) {
+		if (p->next == NULL) { break; }
+		else {
+			p = p->next;
+
+		}
+	}
+	Node *newNode = (Node*)malloc(sizeof(Node));
+	CreateNode(newNode,e);
+	newNode->next = p->next;
+	p->next = newNode;
+	return true;
+
+
+}
+bool DeleteNode(LinkList &list, int i, int &e) {
+	LinkList p = list;
+	for (int j = 0; j < i; j++) {
+		if (p->next == NULL) { return false; }
+		else {
+			p = p->next;
+
+		}
+	}
+	if (p->next->next == NULL) { return false; }
+	p->data = p->next->data;
+	Node *q = p->next;
+	p->next = p->next->next;
+	free(q);
+
+	return true;
+}
+bool InsertByHead(LinkList l , int e) {
+	Node *newNode;
+	CreateNode(newNode,e);
+	newNode->next = l->next;
+	l->next = newNode;
+	return true;
+
+}
+bool InsertByTail(LinkList l, int e) {
+	LinkList p = l;
+	Node *newNode;
+	CreateNode(newNode, e);
+	//找到最后一个结点
+	while (p->next!=NULL)
+	{
+		p = p->next;
+	}
+	newNode->next = p->next;
+	p->next = newNode;
+	return true;
+}
+void ShowLinklist(LinkList l) {
+	LinkList p = l;
+	while (p!=NULL)
+	{
+		cout << p->data << endl;
+		p = p->next;
+	}
+
+}
+int main()
+{
+	LinkList l1;
+	InitList(l1);
+	ListInsert(l1, 0, 1);
+	ListInsert(l1,1, 2);
+	ListInsert(l1, 1, 3);
+	InsertByTail(l1, 5);
+	InsertByHead(l1, 7);
+	ShowLinklist(l1);
+	int i;
+	DeleteNode(l1, 1, i);
+
+	ShowLinklist(l1);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ SqList/SqList/SqList.cpp
@@ -0,0 +1,81 @@
+﻿// SqList.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#define MAXSIZE 10
+using namespace std;
+typedef struct {
+	int *data;
+	int length;
+	int maxSize;
+
+
+}sqList;
+bool InitList(sqList &l) {
+	
+	l.data = (int*)malloc(sizeof(int)*MAXSIZE);
+	l.maxSize = MAXSIZE;
+	for (int i = 0; i < MAXSIZE; i++) {
+		l.data[i] = 3;
+
+	}
+	l.length = MAXSIZE;
+	return true;
+}
+bool ListInsert(sqList &l, int i, int e) {
+	if (i<1 || i>l.length + 1) {
+		return false;
+	}
+	for (int j = l.length; j >= i; j--) {
+		l.data[j] = l.data[j - 1];
+	
+	}
+	l.data[i-1] = e;
+	return true;
+}
+bool ListDelete(sqList &l, int i, int &e) {
+	if (i<1 || i>l.length + 1) {
+		return false;
+	}
+	e = l.data[i-1];
+	for (int j = i; j < l.length; j++) {
+		l.data[j-1] = l.data[j];
+
+	}
+
+	
+	l.length--;
+	return true;
+
+}
+int GetItem(sqList l, int i) {
+	if (i<1 || i>l.length + 1) {
+		return -1;
+	}
+	return l.data[i-1];
+}
+int Locate(sqList l, int e) {
+
+	for (int i = 0; i < l.length; i++) {
+		if (l.data[i] == e) {
+			return i + 1;
+		}
+	
+	}
+	return -1;
+}
+int main() {
+	sqList l;
+	InitList(l);
+	ListInsert(l, 2, 2);
+	int e;
+	ListDelete(l, 1, e);
+	int p = Locate(l, 2);
+	cout << e <<" " <<p<< endl;
+	for (int i = 0; i < MAXSIZE; i++) {
+		cout << l.data[i] << endl;
+	
+	}
+
+
+}
